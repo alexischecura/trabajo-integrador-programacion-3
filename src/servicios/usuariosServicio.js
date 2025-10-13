47ef1781a6b76920c84b080d54a045d37a49f361
Alexis Checura <alexischecura@gmail.com>
2025-10-13 16:59:36 -0300

    agrega conversion de tipo de usuario a entero para guardar en BD

--- src/servicios/usuariosServicio.js
+++ src/servicios/usuariosServicio.js
@@ -1,5 +1,11 @@
 import Usuarios from '../db/usuarios.js';
 
+const TIPOS_DE_USUARIO = {
+  administrador: 1,
+  empleado: 2,
+  cliente: 3,
+};
+
 export default class UsuariosServicio {
   constructor() {
     this.usuarios = new Usuarios();
@@ -16,11 +22,17 @@ export default class UsuariosServicio {
   };
 
   crearUsuario = async (datos) => {
+    datos.tipo_usuario = TIPOS_DE_USUARIO[datos.tipo_usuario] || TIPOS_DE_USUARIO.cliente;
+
     const resultado = await this.usuarios.crearUsuario(datos);
     return resultado.insertId;
   };
 
   actualizarUsuario = async (usuario_id, datos) => {
+    if (datos.tipo_usuario) {
+      datos.tipo_usuario = TIPOS_DE_USUARIO[datos.tipo_usuario] || TIPOS_DE_USUARIO.cliente;
+    }
+
     const usuario = await this.buscarUsuarioPorId(usuario_id);
     if (!usuario) {
       return null;
