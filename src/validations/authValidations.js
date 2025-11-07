4b0f4a4196186a2a82ecddf519497085b2a451de
Alexis Checura <alexischecura@gmail.com>
2025-11-07 16:42:39 -0300

    agrega validación de registro

--- src/validations/authValidations.js
+++ src/validations/authValidations.js
@@ -8,3 +8,26 @@ export const loginValidation = [
     .notEmpty()
     .withMessage('Debe ingresar una contraseña'),
 ];
+
+export const registerValidation = [
+  body('nombre')
+    .notEmpty()
+    .withMessage('El nombre es obligatorio'),
+  body('apellido')
+    .notEmpty()
+    .withMessage('El apellido es obligatorio'),
+  body('nombre_usuario')
+    .isEmail()
+    .withMessage('El nombre de usuario debe tener formato de correo electrónico'),
+  body('contrasenia')
+    .isLength({ min: 6 })
+    .withMessage('La contraseña debe tener al menos 6 caracteres'),
+  body('celular')
+    .optional()
+    .isMobilePhone('es-AR')
+    .withMessage('Número de celular no válido'),
+  body('foto')
+    .optional()
+    .isString()
+    .withMessage('La foto debe ser una cadena de texto'),
+];
